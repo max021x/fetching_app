9f41c86bb3d1b75d3f08cc7a24b7a5a984ccb90b
max <www.max.021persia@gmail.com>
2025-04-07 01:24:17 +0330

    added db fetch and app.js codes from next

---
+++ actions/fetch.js
@@ -0,0 +1,56 @@
+// import {insertcurrency, saveAll } from "@/lib/db";
+
+import { insertCurrency  , insertbBourse , readDate } from "@/lib/db";
+export async function currency(allData) {
+    // take the data form api 
+    const { gold, currency, cryptocurrency } = allData;
+    // check date 
+    const currendate = cryptocurrency[0]['date'] ; 
+    const readTabeldata = await readDate() ; 
+    if(currendate ===  readTabeldata){ 
+        return ; 
+    }    
+
+    // insert data
+    try {
+        // console.log(currency)
+        await currency.map(cur => {
+            insertCurrency(cur.date, +cur.price, cur.symbol);
+        })
+        
+        
+        await gold.map(cur => {
+            const arr = [] 
+            for(let key of Object.keys(cur)){
+                arr.push(cur[key])
+            }
+            arr.push("gold") ;
+            insertbBourse(...arr)  
+        })
+                
+        await currency.map(cur => {
+            const arr = [] 
+            for(let key of Object.keys(cur)){
+                arr.push(cur[key])
+            }
+            arr.push("currency") ; 
+            insertbBourse(...arr) 
+        })
+        
+        await cryptocurrency.map(cur => {
+            const arr = [] 
+            for(let key of Object.keys(cur)){
+                arr.push(cur[key])
+            }
+            arr.push("cryptocurrency") ; 
+            insertbBourse(...arr) ;
+        })
+
+    } catch (error) {
+        console.log(error) ; 
+        return {message:"error"  , error:error}
+    }
+    // redirect or create session 
+}
+
+
--- app.js
+++ app.js
@@ -2,8 +2,22 @@ const express = require('express') ;
 require('dotenv').config() ; 
 const app = express() ; 
 
-
 app.use(express.urlencoded({extended:false})) ; 
+
+
+app.use('/' , (req , res)=>{
+        /* url is wrong */
+        // fetch("https://brsapi.ir/FreeTsetmcBourseApi/Api_Free_Gold_Currency_v2.json")
+        // .then(response=> response.json())
+        // .then(data => console.log(data))
+        // .catch(err=> console.log(err)) ;         
+
+        // reading from db insted 
+
+})
+
+
+
 const port = process.env.APP_PORT ; 
-app.listen(3000 , console.log('server is running on' , `http://127.0.0.1:${port}`));  
+app.listen(port , console.log('server is running on' , `http://127.0.0.1:${port}`));  
 
